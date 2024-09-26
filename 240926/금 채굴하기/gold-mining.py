76de57661b3b6553bcc304ad6e04116e6cdd9e17
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-09-26 18:22:31 +0900

    Title: 금 채굴하기, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240926/금 채굴하기/gold-mining.py
@@ -0,0 +1,32 @@
+n, m = map(int, input().split())
+
+li = [list(map(int, input().split())) for _ in range(n)]
+
+def charge(k):
+    return k**2 + (k+1)**2
+
+def search(r, c, k):
+    num = 0
+    for x in range(r - k, r + k + 1):
+        for y in range(c - k, c + k + 1):
+            if 0 <= x < n and 0 <= y < n and abs(x - r) + abs(y - c) <= k and li[x][y] == 1:
+                num += 1
+    return num
+
+if m > charge(0):
+    answer = 1
+else:
+    answer = 0
+
+k = 1
+while k <= n - 1:
+    for r in range(n):
+        for c in range(n):
+            tmp = search(r, c, k)
+
+            if tmp * m > charge(k) and tmp > answer:
+                answer = tmp
+
+    k += 1
+
+print(answer)
