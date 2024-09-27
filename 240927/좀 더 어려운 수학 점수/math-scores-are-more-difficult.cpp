c00219427dbe563e984fb24dd2d7791321604773
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-09-27 14:06:32 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    if(a > c){
+        cout << "A";
+    }
+    else if (a <c){
+        cout << "B";
+    }
+    else{
+        if(b > d){
+            cout << "A";
+        }
+        else if (b <d){
+            cout << "B";
+        }
+    }
+
+    return 0;
+}
