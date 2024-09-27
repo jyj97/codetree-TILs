65d61de470fbcf5793143345448fa56bab28587f
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-09-27 13:59:26 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240927/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 240927/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -6,13 +6,13 @@ int main() {
     int a,b,c;
     cin >> a >> b >> c;
 
-    if(a < b && a < c){
+    if(a <= b && a <= c){
         cout << a;
     }
-    else if(b < a && b < c){
+    else if(b <= a && b <= c){
         cout <<b;
     }
-    else if (c < b && c < a){
+    else if (c <= b && c <= a){
         cout << c;
     }
     return 0;
