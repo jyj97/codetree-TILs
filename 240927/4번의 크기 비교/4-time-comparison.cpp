2bbb2660417427d1d41f711a2e3f3e53ff1f0630
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-09-27 13:57:08 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+    cin >> a >> b >> c>> d >> e;
+    if(a>b){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+
+    if(a>c){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+
+    if(a>d){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+
+    if(a>e){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+    return 0;
+}
