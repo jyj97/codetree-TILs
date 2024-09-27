6b6c9b9c618832700923c7c20454aea4252ff443
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-09-27 12:53:46 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/데이터 교환/exchange-data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5, b = 6, c = 7;
+
+    int tmp = c;
+    c = b;
+    b = a;
+    a = tmp;
+    cout << a << endl << b << endl << c;
+    return 0;
+}
