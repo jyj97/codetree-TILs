5658535bf1f577639b6c5535fc0bfe0164a949ca
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-09-27 13:53:03 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240927/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    if (n / 3000 >= 1){
+        cout << "book";
+    }
+    else if(n / 1000 >= 1){
+        cout << "mask";
+    }
+    else if(n / 500 >= 1){
+        cout << "pen";
+    }
+    else{
+        cout << "no";
+    }
+    return 0;
+}
