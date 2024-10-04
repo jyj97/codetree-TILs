10ea3300558b2024fb8d9209299681bbaaec68f0
jyj97 <122950904+jyj97@users.noreply.github.com>
2024-10-04 21:12:19 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5080ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 241004/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,28 @@
+dx = (1, 0)
+dy = (0, 1)
+
+n, m = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+def dfs(x, y):
+
+
+    for i in range(2):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if not (0 <= nx < n and 0 <= ny < m):
+            continue
+
+        if graph[nx][ny] != 0:
+            graph[nx][ny] = graph[x][y] + 1
+            dfs(nx, ny)
+
+
+dfs(0,0)
+
+if graph[n - 1][m - 1] != 0 and graph[n - 1][m - 1] > 1:
+    print(1)
+else:
+    print(0)
